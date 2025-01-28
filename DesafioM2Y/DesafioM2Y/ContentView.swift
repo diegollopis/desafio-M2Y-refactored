4736910c5554b3d64454aa46783e860c921d90cc
Diego Llopis <diego.llopis@fornecedores.sicoob.com.br>
2025-01-28 10:38:03 -0300

    project created

---
+++ DesafioM2Y/DesafioM2Y/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesafioM2Y
+//
+//  Created by Diego Janjardi Briz Llopis on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesafioM2Y/DesafioM2Y/DesafioM2YApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioM2YApp.swift
+//  DesafioM2Y
+//
+//  Created by Diego Janjardi Briz Llopis on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DesafioM2YApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
